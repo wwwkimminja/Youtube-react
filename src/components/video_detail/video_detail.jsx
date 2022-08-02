2b6fee947a2bfc30ae2e8280b76cafb144c7214a
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2022-08-03 02:27:30 +0900

    feat: add VideoDetail

--- src/components/video_detail/video_detail.jsx
+++ src/components/video_detail/video_detail.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import styles from './video_detail.module.css'
 
-function VideoDetail() {
-    return (
-        <div>video_detail</div>
-    )
-}
+const VideoDetail = ({ video, video: { snippet } }) => (
+    <section className={styles.detail}>
+        <iframe
+            className={styles.video}
+            type="text/html"
+            width="100%"
+            height="500px"
+            src={`https://www.youtube.com/embed/${video.id}`}
+            frameborder="0"
+            allowfullscreen
+        ></iframe>
+
+
+        <h2>{snippet.title}</h2>
+        <h3>{snippet.channelTitle}</h3>
+        <pre className={styles.description}>{snippet.description}</pre>
+
+    </section>
+);
 
 export default VideoDetail;
