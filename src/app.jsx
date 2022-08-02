3de159251f0c154e5437461cd047f11439053e13
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2022-08-03 02:35:11 +0900

    back to the list when searching

--- src/app.jsx
+++ src/app.jsx
@@ -14,8 +14,13 @@ function App({ youtube }) {
   const search = query => {
     youtube
       .search(query) //
-      .then(videos => setVideos(videos));
+      .then(videos => {
+        setVideos(videos)
+        setSelectedVideo(null);
+      });
+
   };
+
 
   useEffect(() => {
     youtube
