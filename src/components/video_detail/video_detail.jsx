f861bf035e11f52e1677160b92b578b8c7a602cf
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2022-08-04 12:17:59 +0900

    fix:error

--- src/components/video_detail/video_detail.jsx
+++ src/components/video_detail/video_detail.jsx
@@ -4,13 +4,14 @@ import styles from './video_detail.module.css'
 const VideoDetail = ({ video, video: { snippet } }) => (
     <section className={styles.detail}>
         <iframe
+            title={snippet.title}
             className={styles.video}
             type="text/html"
             width="100%"
             height="500px"
             src={`https://www.youtube.com/embed/${video.id}`}
-            frameborder="0"
-            allowfullscreen
+            frameBorder="0"
+            allowFullscreen
         ></iframe>
 
 
